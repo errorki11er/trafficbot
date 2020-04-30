e12179639a0103bd16b7d5d4e02dcb95629510f6
Error killer <63567431+errorki11er@users.noreply.github.com>
2020-04-30 20:02:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,14 +22,14 @@ except:
 
 tr=TorRequest(password=None)
 
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+h = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
 a = input("URL    - ")
 x = int(input("Amount - "))
 
 def run():
 	for i in range(x):
 		tr.reset_identity()
-		response= tr.get(a)
-		print(i+1)
+		response= tr.get(a,headers=h)
+		print("Added",i+1,"Views")
 
 run()
